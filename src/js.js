9ee3b07e367e73801da61b9d3ac1ed32a074d94f
mina-houshmand <houshmand_mina@yahoo.com>
2022-12-11 18:09:02 +0330

    print input value in cityname and add current time

--- src/js.js
+++ src/js.js
@@ -21,17 +21,52 @@ let weather = {
   },
 };
 
-let city = prompt("Enter a acity");
-cityName = city.toLowerCase().trim();
+// let city = prompt("Enter a acity");
+// cityName = city.toLowerCase().trim();
 
-if (cityName) {
-  alert(
-    `It is currently ${Math.round(
-      weather[cityName].temp
-    )}°C in ${cityName} with a humidity of ${weather[cityName].humidity}%`
-  );
-} else {
-  alert(
-    "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
-  );
+// if (cityName) {
+//   alert(
+//     `It is currently ${Math.round(
+//       weather[cityName].temp
+//     )}°C in ${cityName} with a humidity of ${weather[cityName].humidity}%`
+//   );
+// } else {
+//   alert(
+//     "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
+//   );
+// }
+
+//adding current date to app
+
+let date = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = date.getDay();
+let hour = date.getHours();
+let min = date.getMinutes();
+console.log(days[day]);
+let currentDate = document.querySelector("#current-date");
+currentDate.innerHTML = `${days[day]} ${hour}:${min}`;
+
+//addding input value to city-name
+
+function printCityName(event){
+  event.preventDefault();
+  let cityNames = document.querySelector("#get-cityname-input");
+
+  let userCity = document.querySelector("#city-name");
+  userCity.innerHTML= cityNames.value;
+
 }
+
+
+
+let addEventForm = document.querySelector("#get-cityname");
+addEventForm.addEventListener("submit" , printCityName);
